645d6f4269b4074f5501fab6b6fd431933560579
Night-stars-1 <nujj1042633805@gmail.com>
2023-05-27 01:54:03 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,6 @@
  * Copyright (c) 2023 by Night-stars-1, All Rights Reserved. 
  */
 import { defaultTheme } from 'vuepress'
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 export default {
     title: '崩坏：星穹铁道小助手文档',
     description: '崩坏：星穹铁道小助手文档',
@@ -17,59 +16,6 @@ export default {
           lang: 'zh-CN'
         }
     },
-    plugins: [
-        docsearchPlugin({
-            apiKey: "c1b57ecf806bfe5c370d3de23b858065",
-            appId: "M6984MENBN",
-            indexName: "just_laws",
-            searchParameters: {
-              attributesToSnippet: ["lvl1:30", "content:25"],
-            },
-            locales: {
-              "/": {
-                placeholder: "搜索文档",
-                translations: {
-                  button: {
-                    buttonText: "搜索文档",
-                    buttonAriaLabel: "搜索文档",
-                  },
-                  modal: {
-                    searchBox: {
-                      resetButtonTitle: "清除查询条件",
-                      resetButtonAriaLabel: "清除查询条件",
-                      cancelButtonText: "取消",
-                      cancelButtonAriaLabel: "取消",
-                    },
-                    startScreen: {
-                      recentSearchesTitle: "搜索历史",
-                      noRecentSearchesText: "没有搜索历史",
-                      saveRecentSearchButtonTitle: "保存至搜索历史",
-                      removeRecentSearchButtonTitle: "从搜索历史中移除",
-                      favoriteSearchesTitle: "收藏",
-                      removeFavoriteSearchButtonTitle: "从收藏中移除",
-                    },
-                    errorScreen: {
-                      titleText: "无法获取结果",
-                      helpText: "你可能需要检查你的网络连接",
-                    },
-                    footer: {
-                      selectText: "选择",
-                      navigateText: "切换",
-                      closeText: "关闭",
-                      searchByText: "搜索提供者",
-                    },
-                    noResultsScreen: {
-                      noResultsText: "无法找到相关结果",
-                      suggestedQueryText: "你可以尝试查询",
-                      reportMissingResultsText: "你认为该查询应该有结果？",
-                      reportMissingResultsLinkText: "点击反馈",
-                    },
-                  },
-                },
-              },
-            },
-        }),
-    ],
     theme: defaultTheme({
         navbar: [
             { text: "首页", link: "/" },
