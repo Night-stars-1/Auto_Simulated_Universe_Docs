791793e94abcf233f92a42d89344353d978c8736
ZYX <chnzyx2016@gmail.com>
2023-06-14 03:07:15 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ const __dirname = getDirname(import.meta.url)
 
 export default {
     title: 'Auto_Simulated_Universe文档',
-    description: 'Auto_Simulated_Universe文档',
+    description: 'Auto Simulated Universe文档',
     head: [['link', { rel: 'icon', href: 'icon_m.png' }]],
     locales: {
         '/': {
